df04ad6e5b061c45a10e8c56aeaab58460fa4e4c
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-23 18:33:01 +0000

    fix tests

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\Attribute
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+return ['Attribute' => [
+    'AttributeTypes'  => 'Attribute Types',
+    'Attribute'       => 'Attribute',
+    'Attributes'      => 'Attributes',
+    'Name'            => 'Name',
+    'Title'           => 'Title',
+    'Datatype'        => 'Datatype',
+    'Status'          => 'Status',
+    'Type'            => 'Type',
+    'Unit'            => 'Unit',
+    'Value'           => 'Value',
+    'DefaultValues'   => 'Default Values',
+    'Pattern'         => 'Pattern',
+    'IsRequired'      => 'Is required?',
+    'CustomValue'     => 'Custom values allowed?',
+]];
